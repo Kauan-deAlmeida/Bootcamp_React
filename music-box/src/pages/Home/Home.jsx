764294c51b1b454a2bb74ba38e38a36483ec2735
Kauan-deAlmeida <kauan.almeida@sptech.school>
2025-01-08 01:08:11 -0300

    Bootcamp de React, fazendo uma pagina de musica usando CRUD

---
+++ music-box/src/pages/Home/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./Home.module.css";
+import logo from "../../utils/assets/logo.svg";
+import NavBar from "../../components/NavBar/NavBar.jsx";
+import imgPrincipal from "../../utils/assets/img-fundo-principal.png";
+import { useNavigate } from "react-router-dom";
+const Home = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <NavBar logoInicio={logo} />
+      <div className={styles["background-image"]}>
+        <img src={imgPrincipal} alt="Imagem de fundo" />
+        <div className={styles["titulo"]}>
+          <h1>
+            Deixe a música <span> sair da caixa</span>
+          </h1>
+          <button onClick={() =>
+            navigate("/musicas")}>Começar</button>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Home;
